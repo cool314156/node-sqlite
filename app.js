d4a7e6b05d34087d12aba8b06d971d19ebeb7797
cool314156 <35553436@qq.com>
2025-08-21 15:59:06 +0800

    再次修改cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 // 使用cors中间件 解决跨域问题
 app.use(cors({
-  origin: ['http://localhost:8080','https://node-sqlite-cool3141567250-igz9bhp4.leapcell.dev/'], // 允许的前端域名
+  origin: ['http://localhost:8080','https://node-sqlite-cool3141567250-igz9bhp4.leapcell.dev'], // 允许的前端域名
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-token'], // 添加 x-token
   credentials: true
